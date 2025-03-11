443883f27b0276d286953c19693af492b5f40d56
Treshia Banawan <Treshia Banawan@LAPTOP-FC9GSM04>
2025-03-11 15:45:38 +0800

    added a register form mga paking shet

---
+++ Forms/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace warehousesystem.Forms
+{
+    public partial class Register : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+    }
+}
