e97d21f310989f6cd4f03d7f738a7ca378ce5a0c
giecopags <giecoaceo.pagaling@dmc.edu.ph>
2025-05-26 19:04:31 +0800

    dashboard updated

---
+++ Model/BestProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warehousesystem.Model
+{
+    public class BestProduct
+    {
+        public string ProductName { get; set; }
+        public int Rank { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
