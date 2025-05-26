8d7658cab328a91c8759e13dd1319ca96c6c5e16
giecopags <giecoaceo.pagaling@dmc.edu.ph>
2025-05-26 18:22:30 +0800

    global string

--- GlobalConnection/ConnectionString.cs
+++ GlobalConnection/ConnectionString.cs
@@ -11,6 +11,7 @@ namespace warehousesystem.GlobalConnection
         //public static string ConnString = @"Data Source = LAPTOP-NN71FQ8R\SQLEXPRESS;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
         //public static string ConnString = @"Data Source = KWEENI;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
         //public static string ConnString = @"Data Source = LAPTOP-FC9GSM04\SQLEXPRESS;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
-        public static string ConnString = @"Data Source = DESKTOP-JVDG5NL;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
+        //public static string ConnString = @"Data Source = DESKTOP-JVDG5NL;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
+        public static string ConnString = @"Data Source = PAGALING\SQLEXPRESS;Initial Catalog = SmartWareHouseDB;Integrated Security = True;";
     }
 }
