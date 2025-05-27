e16607c1eb36c04534c3e137e440a0af6c951507
ACER <ACER@CLINT>
2025-05-27 11:37:41 +0800

    Reports Forms

---
+++ Reports/MonthlySalesReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace warehousesystem.Reports
+{
+    public partial class MonthlySalesReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public MonthlySalesReport()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Reports/WeeklySalesReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace warehousesystem.Reports
+{
+    public partial class WeeklySalesReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public WeeklySalesReport()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
